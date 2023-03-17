520fc0be8f01fb8dadd8e94f67de29b28dd3b97a
sensational.mack <mackn478@gmail.com>
2023-03-17 12:16:43 -0400

    add input field

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,2 +1,14 @@
+//Declaring the variables
+var characterName;
+var characterAge;
+
+//Assigning values to the variables
 var characterName = "Therapy";
-var characterJob = "Assist with Therapy";
+var characterAge = 35;
+
+console.log(characterAge);
+
+//Conacating the variable and assignments
+console.log(" The character name is " + characterName + " The character age is " + characterAge);
+
+
