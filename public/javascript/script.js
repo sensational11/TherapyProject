6f23d151c0201cd2d61aac1f1e1e854a3e2806c7
sensational.mack <mackn478@gmail.com>
2023-03-20 19:42:45 -0400

    commented out change for search bar

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,14 +1,13 @@
-//Declaring the variables
-var characterName;
-var characterAge;
+// //Declaring the variables
+// var characterName;
+// var characterAge;
 
-//Assigning values to the variables
-var characterName = "Therapy";
-var characterAge = 35;
+// //Assigning values to the variables
+// var characterName = "Therapy";
+// var characterAge = 35;
 
-console.log(characterAge);
+// console.log(characterAge);
 
-//Conacating the variable and assignments
-console.log(" The character name is " + characterName + " The character age is " + characterAge);
-
+// //Conacating the variable and assignments
+// console.log(" The character name is " + characterName + " The character age is " + characterAge);
 
